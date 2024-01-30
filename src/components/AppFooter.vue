1c120c70ac31f2e6af8a81efcca7c69f22c02ded
maicolkutchoukov <maicol.j.95@gmail.com>
2024-01-30 10:02:49 +0100

    Completato footer + una section main e iniziato carosello

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,21 @@
 export default {
     data() {
         return {
-             
+            footerTrainings:[
+                'Riding Lesson',
+                'Safe Driving',
+                'Mountain Bike',
+                'Trail Drive',
+                'Pedaling',
+            ],
+            footerQuickLinks:[
+                'Home',
+                'About Us',
+                'Blog',
+                'Packages',
+                'Contact',
+
+            ]
         };
      },
     methods: {
@@ -12,10 +26,110 @@ export default {
 </script>
 
 <template>
-    <footer>
-        Footer
+    <footer class="bg-black text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div class="footer-image px-5">
+                        <img class="img-fluid" src="../assets/img/logo-footer-gobike.png" alt="">
+                    </div>
+                    <div class="ps-2">
+                        <i class="fa-solid fa-location-dot fa-2xl"></i>
+                        New Jersey, USA
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-phone-volume fa-2xl"></i>
+                        +1(234)567 89 10
+                    </div>
+                    <div class="pb-0">
+                        <i class="fa-regular fa-envelope fa-2xl"></i>
+                        example@expample.com
+                    </div>
+                    <div>
+                        <a href="">
+                            <i class="fa-brands fa-facebook-f fa-2xl"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa-brands fa-instagram fa-2xl"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa-brands fa-twitter fa-2xl" style="color: #ffffff;"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-5 px-5">
+                    <h3 class="pt-5 mb-3">
+                        Join Our Newsletter
+                    </h3>
+                    <p class="mb-5">Subscribe to receive useful information</p>
+                    <input class="py-4 px-4 w-100 rounded mb-1" type="text" placeholder="Your Email Address">
+                    <button class="w-100 py-4 rounded-top">
+                        Subscribe
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+                <div class="col-2 px-3 py-5">
+                    <h3 class="fw-bold mb-4 px-3">Trainings</h3>
+                    <ul class="list-unstyled px-3">
+                        <li class="mb-3" v-for="(elem, i) in footerTrainings">
+                            <a href="">
+                                <span></span> {{ elem }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2 px-3 py-5">
+                    <h3 class="fw-bold mb-4 px-3">Quick Links</h3>
+                    <ul class="list-unstyled px-3">
+                        <li class="mb-3" v-for="(elem, i) in footerQuickLinks">
+                            <a class="mb-4" href="">
+                                <span></span> {{ elem }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center py-4">
+                <i class="fa-regular fa-copyright"></i>
+                2023 Gobike - All Rights Reserved.
+            </div>
+        </div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
+.col-3{
+    padding: 5px 15px;
+}
+.col-3 > *{
+    padding: 20px 5px;
+    i{
+        margin-right: 20px;
+    }
+}
+.col-3 a{
+    text-decoration: none;
+    color: white;
+    border: 1px solid grey;
+    display: inline-block;
+    height: 50px;
+    line-height: 50px;
+    width: calc((100% / 3) - 10px);
+    text-align: center;
+    padding-left: 15px;
+    margin: 5px;
+}
+.col-2 a{
+    text-decoration: none;
+    color: white;
+    
+    span{
+        display: inline-block;
+        width: 10px;
+        height: 4px;
+        background-color: white;
+        margin-bottom: 3px;
+        margin-right: 10px;
+    }
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,17 +2,71 @@
 export default {
     data() {
         return {
-             
+            inactiveCard: 4,
+            events:[
+                {
+                    firstDate: '2022/06/22 - 01:30',
+                    lastDate: '2024/08/23 - 7:00',
+                    name: "Toronto Cycling Event 2023",
+                    city: 'Toronto',
+                    type: 'Bicycle Race',
+
+                },
+                {
+                    firstDate: '2022/06/22 - 01:30',
+                    lastDate: '2024/08/23 - 7:00',
+                    name: "Istanbul Cycling Event 2023",
+                    city: 'Istanbul',
+                    type: 'Bike Sport',
+                },
+                {
+                    firstDate: '2022/06/30 - 01:30',
+                    lastDate: '2024/11/08 - 16:30',
+                    name: "Paris Cycling Event 2023",
+                    city: 'Parisi',
+                    type: 'Cycling Tour',
+
+                },
+                
+                {
+                    firstDate: '2022/06/25 - 03:00',
+                    lastDate: '2024/08/24 - 5:30',
+                    name: "New Jersey Cycling Event 2023",
+                    city: 'New Jersey',
+                    type: 'Bicycle Race',
+
+                }
+            ] 
         };
      },
     methods: {
-
+        prevButtonCard(){
+            console.log(this.inactiveCard)
+            if(this.inactiveCard > 0){
+                this.inactiveCard--
+            } else if (this.inactiveCard == 0){
+                this.inactiveCard = 4
+            } else {
+                this.inactiveCard++
+            }
+        },
+        nextButtonCard(){
+            console.log(this.inactiveCard)
+            if(this.inactiveCard < 4){
+                this.inactiveCard++
+            } else if (this.inactiveCard == 4){
+                this.inactiveCard = 0
+            } else {
+                this.inactiveCard--
+            }
+        }
     }
 }
 </script>
 
 <template>
-    <main>
+    <main class="mb-4">
+        <!-- Primo carosello -->
         <div id="carouselExampleFade" class="carousel slide carousel-fade">
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -69,19 +123,176 @@ export default {
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-        <div class="text-center">
-            <h2>
-                Our Professional Cycling Trainings
-            </h2>
-            <p>
-                Learn from the mountain biking expert.
-            </p>
-            <div class="row">
-                <div class="col-12">
-                    
+        <section class="my-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 biker-circle d-flex justify-content-center">
+                        <img class="img-fluid reverse rotate" src="../assets/img/bike-circle.png" alt="">
+                        <img class="img-fluid" src="../assets/img/bike-player.png" alt="">
+                        
+                    </div>
+                    <div class="col-4">
+                        <h2 class="fs-2 fw-bold mb-4">
+                            Do you want to be a professional cyclist?
+                        </h2>
+                        <p class="mb-3">
+                            We offer 6 courses of varying difficulty and beautiful scenery that cyclists of all levels can enjoy. 
+                            You will learn cycling from professionals with our competent and experienced staff. 
+                            You will have a lot of fun with our amazing space. <br>
+                        </p>
+                        <img class="mb-5" src="../assets/img/divider.jpg" alt="">
+                        <div class="row">
+                            <div class="col-4">
+                                <img src="https://img.freepik.com/premium-vector/full-face-helmet-motocross-downhill-mtb-hemet-vector-sketch-ilustration_379823-644.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-8">
+                                <h3 class="fw-bold">Professional Team</h3>
+                                <p class="">
+                                    We make you professional by adding your sincerity as well as professionalism.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row mb-4">
+                            <div class="col-4">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiEkXijI-Wb0tmGAK8AhkPkBdb_qm_8FCF5w&usqp=CAU" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-8">
+                                <h3 class="fw-bold">Professional Trainings</h3>
+                                <p class="">
+                                    We always ride with the best equipment, respecting nature and fun.
+                                </p>
+                            </div>
+                        </div>
+                        <a class="btn-know" href="#">
+                            Get to know us 
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+         <!-- Secondo carosello -->
+         <section class="mb-5">
+            <h2 class="text-center fw-bold mt-4 mb-2">
+                Our Professional Cycling Trainings
+            </h2>
+            <p class="text-center mb-4">
+                Learn from the mountain biking expert.
+            </p>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <div class="carousel-container d-flex flex-wrap justify-content-between pb-5">
+                            <div v-if="inactiveCard != 0" class="carousel_card">
+                                <img class="img-fluid" src="../assets/img/training-box-1.jpg" alt="Training-1">
+                                <div class="card_content">
+                                    <h3>Riding Lesson</h3>
+                                    <a href="#">
+                                        View
+                                    </a>
+                                </div>
+                            </div>
+                            <div v-if="inactiveCard != 1" class="carousel_card">
+                                <img class="img-fluid" src="../assets/img/training-box-2.jpg" alt="Training-2">
+                                <div class="card_content">
+                                    <h3>Safe Driving</h3>
+                                    <a href="#">
+                                        View
+                                    </a>
+                                </div>
+                            </div>
+                            <div v-if="inactiveCard != 2" class="carousel_card">
+                                <img class="img-fluid" src="../assets/img/training-box-3.jpg" alt="Training-3">
+                                <div class="card_content">
+                                    <h3>Mountain Bike</h3>
+                                    <a href="#">
+                                        View
+                                    </a>
+                                </div>
+                            </div>
+                            <div v-if="inactiveCard != 3" class="carousel_card">
+                                <img class="img-fluid" src="../assets/img/training-box-4.jpg" alt="Training-4">
+                                <div class="card_content">
+                                    <h3>Trail Drive</h3>
+                                    <a href="#">
+                                        View
+                                    </a>
+                                </div>
+                            </div>
+                            <div v-if="inactiveCard != 4" class="carousel_card">
+                                <img class="img-fluid" src="../assets/img/training-box-5.jpg" alt="Training-5">
+                                <div class="card_content">
+                                    <h3>Pedaling</h3>
+                                    <a href="#">
+                                        View
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="button_prev" @click="prevButtonCard()">
+                        <i class="fa-solid fa-chevron-left"></i>
+                    </button>
+                    <button class="button_next ms-4" @click="nextButtonCard()">
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+        </section>
+        <section class="text-center">
+            <div class="container">
+                <h3 class="fw-bold">
+                Resent New & Articles
+            </h3>
+            <p>Important information about bike</p>
+            <div class="row">
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/news-bike5-300x180.jpg" class="card-img-top px-4 pt-4" alt="...">
+                        <div>22.06.2022 - Bike</div>
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">Road bike or mountain bike?</h5>
+                            <p class="card-text">Comparated to similar road bikes with a solid fram structure [...]</p>
+                            <a href="#" class="btn btn-dark px-4 py-2">More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/news-bike6-300x180.jpg" class="card-img-top px-4 pt-4" alt="...">
+                        <div>22.06.2022 - Bike</div>
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">What is mountain biking called?</h5>
+                            <p class="card-text">Mountain biking is one of the most popolar outdoors sports, [...]</p>
+                            <a href="#" class="btn btn-dark px-4 py-2">More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/news-bike3-300x180.jpg" class="card-img-top px-4 pt-4" alt="...">
+                        <div>22.06.2022 - Bike</div>
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">How much should you cycle a day?</h5>
+                            <p class="card-text">In order to get the right benefit from the exercise [...]</p>
+                            <a href="#" class="btn btn-dark px-4 py-2">More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/news-bike4-300x180.jpg" class="card-img-top px-4 pt-4" alt="...">
+                        <div>22.06.2022 - Bike</div>
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">How long does it take 5 km by bike</h5>
+                            <p class="card-text">It takes 5 km and 10 minutes, sir. But at [...]</p>
+                            <a href="#" class="btn btn-dark px-4 py-2">More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -123,6 +334,88 @@ export default {
         background-color: black;
         padding: 25px 50px;
         margin-top: 50px;
+    }
+}
+.biker-circle{
+    position: relative;
+
+}
+.biker-circle img:first-child{
+    position: absolute;
+    z-index: 0;
+    top: 10px;
+    right: 35px;
+}
+.biker-circle img:last-child{
+    z-index: 10;
+}
+
+.rotate {
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  animation-name: rotate;
+  animation-timing-function: linear;
+}
+
+@keyframes rotate {
+  from { transform: rotate(  0deg); }
+  to   { transform: rotate(360deg); }
+}
+
+.btn-know{
+    background-color: black;
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+    padding: 25px 40px;
+}
+.carousel_card{
+    width: calc(100% / 5);
+    margin: 0 15px;
+    border: 6px solid black;
+    position: relative;
+    .card_content{
+        position: absolute;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.4);
+        h3{
+            font-weight: bold;
+            font-size: 1.1em;
+            color: white;
+            padding: 15px;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            font-weight: bold;
+            background-color: black;
+            padding: 10px 20px;
+            margin: 15px;
+            position: absolute;
+            bottom: 10px;
+            display: none;
+        }
+        &:hover{
+                background-color: rgb(0, 0, 0, 0.25);
+                transition: all 0.3s ease ;
+                & a{
+                    display: block;
+                }
+            }
+    }
+}
+
+.button_prev, .button_next{
+    width: 60px;
+    height: 60px;
+    line-height: 60px;
+    font-size: 1.2em;
+    border: 2px solid black;
+    &:hover{
+        background-color: grey;
+        cursor: pointer;
     }
 }
 </style>
